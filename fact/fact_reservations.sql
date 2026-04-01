69e6f5c8d6e32fb94ba22945213e9980a1dc6bb0
MarCorri <mmc.corriero@gmail.com>
2026-04-01 19:19:06 +0200

    Create fact_reservations.sql

---
+++ fact/fact_reservations.sql
@@ -0,0 +1,16 @@
+CREATE TABLE fact_reservations AS
+SELECT
+    hotel_id,
+    reservation_code,
+    booking_date,
+    check_in_date,
+    check_out_date,
+    reservation_status,
+    agency_name,
+    market_source,
+    market_segment,
+    guest_count,
+    rate_plan_code,
+    daily_rate_amount,
+    total_amount
+FROM stg_reservations;
